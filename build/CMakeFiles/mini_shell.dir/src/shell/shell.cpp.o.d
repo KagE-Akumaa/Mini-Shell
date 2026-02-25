a96f7833964dc67351e0e7e77ed5bf48c4fbf5e6
KagE-Akumaa <mukuljogi18@gmail.com>
2026-02-25 22:09:35 +0530

    remove unwantde files

--- build/CMakeFiles/mini_shell.dir/src/shell/shell.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/mini_shell.dir/src/shell/shell.cpp.o: \
- /home/akumaa/projects/Mini-Shell/src/shell/shell.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/mini_shell.dir/src/utils/utils.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/mini_shell.dir/src/utils/utils.cpp.o: \
- /home/akumaa/projects/Mini-Shell/src/utils/utils.cpp \
- /usr/include/stdc-predef.h
